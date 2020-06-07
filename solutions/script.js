ff582b03933459b8e0708da0a42dfec8cb3b32d4
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-06-06 23:02:31 -0700

    Add event listeners

--- solutions/script.js
+++ solutions/script.js
@@ -1,8 +1,42 @@
 console.log("script.js");
 
 function getHistory() {
-  console.log(document.getElementById("history-value"));
   return document.getElementById("history-value").innerText;
 }
 
 alert(getHistory());
+
+function printHistory(num) {
+  document.getElementById("history-value").innerText = num;
+}
+function getOutput() {
+  return document.getElementById("output-value").innerText;
+}
+function printOutput(num) {
+  if (num == "") {
+    document.getElementById("output-value").innerText = num;
+  } else {
+    document.getElementById("output-value").innerText = getFormattedNumber(num);
+  }
+}
+
+function getFormattedNumber(num) {
+  let n = Number(num);
+  let value = n.toLocaleString("en");
+  return value;
+}
+
+function reverseNumberFormat(num) {
+  return Number(num.replace(/,/g, ""));
+}
+
+printOutput("9089332");
+alert(reverseNumberFormat(getOutput()));
+
+let operator = document.getElementsByClassName("operator");
+console.log(operator);
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    alert("The oprator clicked:" + this.id);
+  });
+}
