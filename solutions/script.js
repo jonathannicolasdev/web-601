916b629467a3f6e9d3baa4fd713639089b7529d3
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-06-08 09:06:26 -0700

    latest

--- solutions/script.js
+++ solutions/script.js
@@ -20,21 +20,43 @@ function printOutput(num) {
   }
 }
 
+printOutput("5555");
+
 function getFormattedNumber(num) {
   let n = Number(num);
   let value = n.toLocaleString("en");
   return value;
 }
 
+console.log(getFormattedNumber("7888"));
+
 function reverseNumberFormat(num) {
   return Number(num.replace(/,/g, ""));
 }
+
+let revNum = reverseNumberFormat("9,999");
+console.log(revNum);
 
 printOutput("9089332");
 alert(reverseNumberFormat(getOutput()));
 
 let operator = document.getElementsByClassName("operator");
+
+console.log("these are the operator buttons", operator);
+
 console.log(operator);
+console.log(operator.length);
+//             0         1         2
+let fruits = ["apple", "orange", "banana"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits.length);
+
+for (var i = 0; i < fruits.length; i++) {
+  console.log(fruit[i]);
+}
+
 for (var i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", function () {
     alert("The oprator clicked:" + this.id);
